f1bb6afd46ddef7fec09031ed46f3f169cbbea24
lorensansol <lorensansol@gmail.com>
2023-12-06 03:39:02 +0100

    Update: 2023-12-06 03:39:02

--- assets/js/custom.js
+++
@@ -1,7 +0,0 @@
-
-// FOOTER
-const footerLastItem = document.querySelector('.footer__item:last-child')
-if (footerLastItem) {
-  footerLastItem.innerHTML = footerLastItem.innerHTML.replace('Diseño', 'Desarrollo')
-  footerLastItem.insertAdjacentHTML('beforebegin', '<div class="footer__item">Diseño Web J. Campos</div>' )
-}
